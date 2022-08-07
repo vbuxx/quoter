ca9ce2508646eec6a2f687c49e4f241536043bd1
Andhika Pramana <andhikapramana101@gmail.com>
2022-08-07 11:12:35 +0700

    Create Quoter functionality

---
+++ src/Quoter.jsx
@@ -0,0 +1,62 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "./Quoter.css";
+
+const quotableAPI = "https://api.quotable.io/random";
+
+const Quoter = () => {
+  const [quote, setQuote] = useState({
+    content: "",
+    author: "",
+  });
+
+  const handleClick = () => {
+    axios.get(quotableAPI).then((response) => {
+      setQuote({
+        content: response.data.content,
+        author: response.data.author,
+      });
+    });
+  };
+
+  useEffect(() => {
+    document.title = "Quoter";
+  });
+
+  return (
+    <>
+      <div id="quote-box">
+        <div className="text-wrapper">
+          <p id="text">{quote.content}</p>
+          <h4 id="author">{quote.author}</h4>
+        </div>
+        <div className="button-wrap">
+          <button id="new-quote" onClick={handleClick}>
+            New Quote!
+          </button>
+          <button className="twit-btn">
+            <a
+              href={
+                "https://twitter.com/intent/tweet?text=" +
+                encodeURIComponent('"' + quote.content + '" ' + quote.author)
+              }
+              id="tweet-quote"
+            >
+              <span>Tweet!</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+              </svg>
+            </a>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Quoter;
